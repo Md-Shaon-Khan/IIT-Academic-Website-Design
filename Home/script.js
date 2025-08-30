04f7f92f294a0fdda474fb3ad018249a8111fa9c
Md-Shaon-Khan <shaon.iit52@gmail.com>
2025-08-30 14:45:21 +0600

    Updated code of HTML,CSS,JS

--- Home/script.js
+++ Home/script.js
@@ -15,3 +15,31 @@ faqEntries.forEach(entry => {
     });
   });
 });
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const icons = document.querySelectorAll(".about__icon");
+
+//   window.addEventListener("scroll", () => {
+//     const rotation = window.scrollY % 360; // scroll position mapped to degrees
+//     icons.forEach((icon, i) => {
+//       // each icon rotates slightly offset
+//       icon.style.transform = `rotate(${rotation + i * 60}deg)`;
+//     });
+//   });
+// });
+
+const iconButtons = document.querySelectorAll(".about__icon-btn");
+
+iconButtons.forEach(btn => {
+  btn.addEventListener("mouseenter", () => {
+    // Get the corresponding text id
+    const textId = "text-" + btn.dataset.text;
+    const textElement = document.getElementById("text-" + btn.dataset.text);
+    
+    // Add active class to show text
+    if (!textElement.classList.contains("active")) {
+      textElement.classList.add("active");
+    }
+  });
+});
